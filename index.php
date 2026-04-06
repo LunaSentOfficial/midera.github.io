17bd2d030537b2b7303ff0e5a1a24487627d06b8
StarMint <hotwheelsarebest@gmail.com>
2026-04-06 16:10:35 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php' ?>
+<div id="content">
+	<p>insert text here</p>
+	<div class="video">
+		<iframe width="900" height="506" src="https://www.youtube.com/embed/shcAvnYp9ow?ecver=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+	</div>
+</div>
+<?php include 'footer.php' ?>
